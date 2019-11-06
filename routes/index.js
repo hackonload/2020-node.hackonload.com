ac9445aa4e530a13ba1565faa5005affaffe1e99
Pushkar Anand <anandpushkar088@gmail.com>
2019-11-06 23:24:53 +0530

    fixed.

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', (req, res, next) => {
 	const url = req.protocol + "://" + req.headers.host + req.originalUrl;
 	const data = {
 		title: 'OnLoad 2.0 | 28th Feb - 1st March',
-		meta_description: 'A 30hrs intense, fun-filled, rewarding convergence of programmers, designers and developers to build something amazing.',
+		meta_description: 'A 36hrs intense, fun-filled, rewarding convergence of programmers, designers and developers to build something amazing.',
 		page_url: url,
 		FAQs: FAQs
 	};
@@ -18,7 +18,7 @@ router.get('/team', (req, res) => {
 	const url = req.protocol + "://" + req.headers.host + req.originalUrl;
 	const data = {
 		title: 'Team behind OnLoad 2.0',
-		meta_description: 'A 30hrs intense, fun-filled, rewarding convergence of programmers, designers and developers to build something amazing.',
+		meta_description: 'A 36hrs intense, fun-filled, rewarding convergence of programmers, designers and developers to build something amazing.',
 		page_url: url
 	};
 	res.render('team', data);
