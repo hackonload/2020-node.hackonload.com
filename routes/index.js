5c654362cf305d8c302fdcb3c7e8ceba61c665ea
Pushkar Anand <anandpushkar088@gmail.com>
2019-11-07 20:47:48 +0530

    corrected error in url.

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/team', (req, res) => {
-	const url = req.protocol + "://" + req.headers.host + req.originalUrl;
+	const url = req.protocol + "://" + "hackonload.com" + req.originalUrl;
 	const data = {
 		title: 'Team behind OnLoad 2.0',
 		meta_description: 'A 36hrs intense, fun-filled, rewarding convergence of programmers, designers and developers to build something amazing.',
