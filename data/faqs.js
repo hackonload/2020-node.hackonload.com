d15d4a599cfc490b1a7cb7018bbd0cf9b8e0e7a8
Pushkar Anand <anandpushkar088@gmail.com>
2019-12-22 20:25:58 +0530

    FAQ updates.

--- data/faqs.js
+++ data/faqs.js
@@ -25,44 +25,44 @@ const FAQs = [
 		answer:"Weapons of any kind, drugs, or alcohol. If you’re not sure whether something will be okay, please ask ahead of time!"
 	},
 	{
-		id:"faq-6",
-		question:"What about Hardware?",
-		answer:"If you need any hardware mail or tweet us about it. We will try our best to arrange all your hardware needs."
+		id: "faq-6",
+		question: "What about Hardware?",
+		answer: "If you need any hardware mail or tweet us about it. We will try our best to arrange all your hardware needs."
 	},
 	{
-		id:"faq-7",
-		question:"I don't want to hack, but i still get involved?",
-		answer:"If you're looking to be a volunteer or mentor (i.e. someone with technical skills who'd be willing to help out participants during the weekend), you should reach out to us at volunteer@hackonload.com. If your company is interested in becoming a sponsor, write to us at sponsor@hackonload.com"
-	},
-	/*	{
-			id:"faq-8",
-			question:"When will application close?",
-			answer:"The application portal will be closed on March 1st at 11:59pm IST. We evaluate applications on a rolling basis and acceptances will be sent every weekend via email. The sooner you apply, the better chances you have of acceptance. Once accepted, you’ll have two days to confirm your attendance"
-		},*/
-	{
-		id:"faq-9",
-		question:"How much does it cost? ",
-		answer:"Zero. Zip. Zilch. Nada. Nothing. Admission to OnLoad is completely free, thanks to our sponsors! We'll provide you with a weekend's worth of schwags, meals, drinks, snacks and a place to crash when you need a break from coding."
+		id: "faq-7",
+		question: "I don't want to hack, but i still get involved?",
+		answer: "If you're looking to be a volunteer or mentor (i.e. someone with technical skills who'd be willing to help out participants during the weekend), you should reach out to us at volunteer@hackonload.com. If your company is interested in becoming a sponsor, write to us at sponsor@hackonload.com"
 	},
 	{
-		id:"faq-10",
-		question:"How big a team can be?",
-		answer:"You can form teams of up to 3 people. Most teams aim to have a mix of people with both design and developer skills."
+		id: "faq-8",
+		question: "When will application close?",
+		answer: "The application portal will be closed on 10th Feb, 2020 at 11:59pm IST. We evaluate applications on a rolling basis and acceptances will be sent every weekend via email. The sooner you apply, the better chances you have of acceptance. Once accepted, you’ll have two days to confirm your attendance"
 	},
-	/*	{
-			id:"faq-11",
-			question:" Once i am accepted, what do i need to bring? ",
-			answer:"A valid government & a college issued photo ID, laptop, phone, chargers, any sleeping equipment you might want (we'll be providing mattresses)"
-		},*/
+	{
+		id: "faq-9",
+		question: "How much does it cost? ",
+		answer: "Zero. Zip. Zilch. Nada. Nothing. Admission to OnLoad is completely free, thanks to our sponsors! We'll provide you with a weekend's worth of schwags, meals, drinks, snacks and a place to crash when you need a break from coding."
+	},
+	{
+		id: "faq-10",
+		question: "How big a team can be?",
+		answer: "You can form teams of up to 3 people. Most teams aim to have a mix of people with both design and developer skills."
+	},
+	{
+		id: "faq-11",
+		question: " Once i am accepted, what do i need to bring? ",
+		answer: "A valid government & a college issued photo ID, laptop, phone, chargers, any sleeping equipment you might want (we'll be providing mattresses)"
+	},
 	{
 		id: "faq-12",
 		question: "Do you provide travel reimbursement",
 		answer: "No, we won't be able to provide reimbursements."
 	},
 	{
-		id:"faq-13",
-		question:"What do you get by attending OnLoad? ",
-		answer:"Pretty much everything from socializing with fellow hacker community to internship/job offers from the coolest tech companies. And yeah we’ll have prizes for finalist teams, and many other company sponsored prizes. Most of all, you will gain an unparalleled learning experience and memories for a lifetime!"
+		id: "faq-13",
+		question: "What do you get by attending OnLoad? ",
+		answer: "Pretty much everything from socializing with fellow hacker community to internship/job offers from the coolest tech companies. And yeah we’ll have prizes for finalist teams, and many other company sponsored prizes. Most of all, you will gain an unparalleled learning experience and memories for a lifetime!"
 	},
 	{
 		id: "faq-14",
