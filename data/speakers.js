4c4cff0f44b3601a027000ee44a37f9786bc02d6
Pushkar Anand <anandpushkar088@gmail.com>
2020-01-13 19:50:04 +0530

    added speaker section data and design.

---
+++ data/speakers.js
@@ -0,0 +1,11 @@
+const speakers = [
+	{
+		name: "Vikram Sahu",
+		designation: "Developer Evangelist",
+		company: "Pepipost",
+		image: "vikram.jpg",
+		link: "https://www.linkedin.com/in/vikram-sahu-6a01987b/"
+	}
+];
+
+module.exports = speakers;
